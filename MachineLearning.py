754b7ee9ed18e5ff15ae3ae2ff54bbbedeebc2cf
chisoftltd <chisoftltd@gmail.com>
2021-03-14 15:44:23 +0100

    Machine Learning - Linear Regression

--- MachineLearning.py
+++ MachineLearning.py
@@ -66,3 +66,62 @@ x = numpy.random.normal(50.0, 10.0, 10000)
 y = numpy.random.normal(100.0, 20.0, 10000)
 plt.scatter(x, y)
 plt.show()
+
+# Machine Learning - Linear Regression
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.show()
+
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+slope, intercept, r, p, std_err = stats.linregress(y, x)
+
+def myfunc(y):
+  return slope * y + intercept
+
+mymodel = list(map(myfunc, y))
+
+plt.scatter(y, x)
+plt.plot(y, mymodel)
+plt.show()
+
+# R for Relationship
+print()
+print(r)
+print()
+print(p)
+print()
+print(std_err)
+print()
+
+# Predict Future Values
+speed = myfunc(100)
+print(speed)
+
+# Bad Fit?
+x = [89,43,36,36,95,10,66,34,38,20,26,29,48,64,6,5,36,66,72,40]
+y = [21,46,3,35,67,95,53,72,58,10,26,34,90,33,38,20,56,2,47,15]
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.show()
+print()
+print(r)
