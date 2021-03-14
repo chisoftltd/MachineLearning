beddda19af474e09ab15f3c9ce8e88aae08c3e78
chisoftltd <chisoftltd@gmail.com>
2021-03-14 01:32:38 +0100

    first commit

---
+++ MachineLearning.py
@@ -0,0 +1,17 @@
+# Machine Learning - Mean Median Mode
+# Mean
+import numpy
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+x = numpy.mean(speed)
+print(x)
+print()
+
+# Median
+x = numpy.median(speed)
+print(x)
+print()
+
+# Mode
+from scipy import stats  
+x = stats.mode(speed)
+print(x)
