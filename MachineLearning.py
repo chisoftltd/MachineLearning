93b9197d96cbd08b71ff56d0d9e5f917ef3b419a
chisoftltd <chisoftltd@gmail.com>
2021-03-14 17:26:14 +0100

    Machine Learning - Scale

--- MachineLearning.py
+++ MachineLearning.py
@@ -158,3 +158,24 @@ plt.scatter(x, y)
 plt.plot(myline, mymodel(myline))
 plt.show()
 print(r2_score(y, mymodel(x)))
+print()
+
+# Machine Learning - Multiple Regression
+import pandas
+from sklearn import linear_model
+df = pandas.read_csv("cars.csv")
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+#predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300cm3:
+predictedCO2 = regr.predict([[2300, 1300]])
+print(predictedCO2)
+print()
+
+
+# Coefficient
+print(regr.coef_)
+print()
