0984479ef18c9ecb13cad1492d9f3e7e92a082f4
chisoftltd <chisoftltd@gmail.com>
2021-03-14 15:01:21 +0100

    Machine Learning - Scatter Plot

--- MachineLearning.py
+++ MachineLearning.py
@@ -35,4 +35,34 @@ print()
 
 x = numpy.percentile(ages, 95)
 print(x)
-print( )
+print()
+
+# Machine Learning - Data Distribution
+x = numpy.random.uniform(0.0, 100.0, 50000)
+print(x)
+print()
+
+# Histogram
+import matplotlib.pyplot as plt
+x = numpy.random.uniform(0.0, 100.0, 50000)
+plt.hist(x, 500)
+plt.show()
+
+# Machine Learning - Normal Data Distribution
+x = numpy.random.normal(15.0, 1.0, 1000000)
+print(x)
+plt.hist(x, 1000)
+plt.show()
+print()
+
+# Machine Learning - Scatter Plot
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6,15,20]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86,92,100]
+plt.scatter(x,y)
+plt.show()
+
+# Random Data Distributions
+x = numpy.random.normal(50.0, 10.0, 10000)
+y = numpy.random.normal(100.0, 20.0, 10000)
+plt.scatter(x, y)
+plt.show()
